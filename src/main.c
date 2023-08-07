8ed5c582f36c67d0d191180760f10395a7e5ab26
rndm <rndm@localhost.localdomain>
2023-08-07 19:36:52 +0000

    Added shooting

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef Vector2 Velocity;
 typedef Vector2 Position;
@@ -74,113 +75,130 @@ void Draw(ecs_iter_t *it) {
 }
 
 int main(void) {
-  const int screenWidth = 1360;
-  const int screenHeight = 700;
+    const int screenWidth = 1360;
+    const int screenHeight = 700;
 
-  InitWindow(screenWidth, screenHeight, "gaming game gamers");
-  ecs_world_t *ecs = ecs_init();
+    InitWindow(screenWidth, screenHeight, "gaming game gamers");
+    ecs_world_t *ecs = ecs_init();
 
-  SetTargetFPS(60);
-  // ecs_set_target_fps(ecs, 60);
+    SetTargetFPS(60);
+    // ecs_set_target_fps(ecs, 60);
 
-  Camera2D camera = {
-      .zoom = 1.,
-      .offset = {screenWidth / 2., screenHeight / 2.},
-      .target = {0., 0.},
-      .rotation = 0.,
-  };
+    Camera2D camera = {
+        .zoom = 1.,
+        .offset = {screenWidth / 2., screenHeight / 2.},
+        .target = {0., 0.},
+        .rotation = 0.,
+    };
 
-  ECS_COMPONENT(ecs, Position);
-  ECS_COMPONENT(ecs, Velocity);
-  ECS_COMPONENT(ecs, Rotation);
-  ECS_COMPONENT(ecs, Scale);
-  ECS_COMPONENT(ecs, Animation);
+    ECS_COMPONENT(ecs, Position);
+    ECS_COMPONENT(ecs, Velocity);
+    ECS_COMPONENT(ecs, Rotation);
+    ECS_COMPONENT(ecs, Scale);
+    ECS_COMPONENT(ecs, Animation);
 
-  ECS_SYSTEM(ecs, Move, EcsOnUpdate, Position, Velocity, Rotation);
-  ECS_SYSTEM(ecs, Draw, EcsOnUpdate, Position, Rotation, Scale, Animation);
+    ECS_SYSTEM(ecs, Move, EcsOnUpdate, Position, Velocity, Rotation);
+    ECS_SYSTEM(ecs, Draw, EcsOnUpdate, Position, Rotation, Scale, Animation);
 
-  ecs_entity_t player = ecs_new_id(ecs);
-  ecs_set(ecs, player, Position, {0, 0});
-  ecs_set(ecs, player, Velocity, {0, 0});
-  ecs_set(ecs, player, Rotation, {0});
-  ecs_set(ecs, player, Scale, {5});
-  
-  Animation starship = {
-      .sheet = LoadTexture(ASSET "starship.png"),
-      .cur_frame = 0,
-      .frame_width = 16,
-      .time = 0,
-      .fps = 8,
-  };
+    ecs_entity_t player = ecs_new_id(ecs);
+    ecs_set(ecs, player, Position, {0, 0});
+    ecs_set(ecs, player, Velocity, {0, 0});
+    ecs_set(ecs, player, Rotation, {0});
+    ecs_set(ecs, player, Scale, {5});
 
-  ecs_set_ptr(ecs, player, Animation, &starship);
+    Animation a_starship = {
+        .sheet = LoadTexture(ASSET "starship.png"),
+        .cur_frame = 0,
+        .frame_width = 16,
+        .time = 0,
+        .fps = 8,
+    };
 
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(WHITE);
-    BeginMode2D(camera);
+    Animation a_laser = {
+        .sheet = LoadTexture(ASSET "laser.png"),
+        .cur_frame = 0,
+        .frame_width = 16,
+        .time = 0,
+        .fps = 1,
+    };
 
-    const float dt = GetFrameTime();
-    ecs_progress(ecs, dt);
+    ecs_set_ptr(ecs, player, Animation, &a_starship);
 
-    { // player movement
-      static Velocity player_vel;
-      player_vel = *ecs_get(ecs, player, Velocity);
-      bool changed = false;
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        BeginMode2D(camera);
 
-      if (IsKeyDown(KEY_RIGHT)) {
-        player_vel.x = Clamp(player_vel.x + 200, -200, 200);
-        changed = true;
-      }
-      if (IsKeyDown(KEY_LEFT)) {
-        player_vel.x = Clamp(player_vel.x - 200, -200, 200);
-        changed = true;
-      }
-      if (IsKeyDown(KEY_UP)) {
-        player_vel.y = Clamp(player_vel.y - 200, -200, 200);
-        changed = true;
-      }
-      if (IsKeyDown(KEY_DOWN)) {
-        player_vel.y = Clamp(player_vel.y + 200, -200, 200);
-        changed = true;
-      }
+        const float dt = GetFrameTime();
+        ecs_progress(ecs, dt);
 
-      if (!changed) { // Slow down if no movement keys are pressed
-        player_vel.x = Lerp(player_vel.x, 0, 0.3);
-        player_vel.y = Lerp(player_vel.y, 0, 0.3);
-      }
+        static Position player_pos;
+        player_pos = *ecs_get(ecs, player, Position);
+        static Velocity player_vel;
+        player_vel = *ecs_get(ecs, player, Velocity);
 
-      ecs_set_ptr(ecs, player, Velocity, &player_vel);
+        { // player movement
+            bool changed = false;
+
+            if (IsKeyDown(KEY_RIGHT)) {
+                player_vel.x = Clamp(player_vel.x + 200, -200, 200);
+                changed = true;
+            }
+            if (IsKeyDown(KEY_LEFT)) {
+                player_vel.x = Clamp(player_vel.x - 200, -200, 200);
+                changed = true;
+            }
+            if (IsKeyDown(KEY_UP)) {
+                player_vel.y = Clamp(player_vel.y - 200, -200, 200);
+                changed = true;
+            }
+            if (IsKeyDown(KEY_DOWN)) {
+                player_vel.y = Clamp(player_vel.y + 200, -200, 200);
+                changed = true;
+            }
+
+            if (!changed) { // Slow down if no movement keys are pressed
+                player_vel.x = Lerp(player_vel.x, 0, 0.3);
+                player_vel.y = Lerp(player_vel.y, 0, 0.3);
+            }
+
+            ecs_set_ptr(ecs, player, Velocity, &player_vel);
+        }
+
+        { // Dynamic camera
+            camera.target = Vector2Lerp(camera.target, player_pos, 1 * dt);
+        }
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            ecs_entity_t laser = ecs_new_id(ecs);
+
+            const Rotation* rot = ecs_get(ecs, player, Rotation);
+            ecs_set_ptr(ecs, laser, Rotation, rot);
+            
+            Velocity vel = Vector2Rotate((Vector2){0, -1000}, *ecs_get(ecs, player, Rotation));
+            ecs_set_ptr(ecs, laser, Velocity, &vel);
+            
+            Velocity init_vel = Vector2Rotate((Vector2){0, -50}, *ecs_get(ecs, player, Rotation));
+            Position pos = Vector2Add(init_vel, *ecs_get(ecs, player, Position));
+            ecs_set_ptr(ecs, laser, Position, &pos);
+            
+            ecs_set(ecs, laser, Scale, {3});
+
+            ecs_set_ptr(ecs, laser, Animation, &a_laser);
+        }
+
+        EndMode2D();
+
+        { // UI
+            DrawFPS(5, 5);
+
+
+        }
+        EndDrawing();
     }
 
-    float wheel = GetMouseWheelMove();
+    CloseWindow(); // Close window and OpenGL context
+    ecs_fini(ecs);
 
-    if (wheel != 0) {
-      // Get the world point that is under the mouse
-      Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
-
-      // Set the offset to where the mouse is
-      camera.offset = GetMousePosition();
-
-      // Set the target to match, so that the camera maps the world space point
-      // under the cursor to the screen space point under the cursor at any zoom
-      camera.target = mouseWorldPos;
-
-      // Zoom increment
-      const float zoomIncrement = 50 * dt;
-
-      camera.zoom += (wheel * zoomIncrement);
-      if (camera.zoom < zoomIncrement)
-        camera.zoom = zoomIncrement;
-    }
-
-    EndMode2D();
-    DrawFPS(5, 5);
-    EndDrawing();
-  }
-
-  CloseWindow(); // Close window and OpenGL context
-  ecs_fini(ecs);
-
-  return 0;
+    return 0;
 }
