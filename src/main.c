2c966e4323c4c4f95cafe9b4f2fbc2d173f4020c
rndm <rndm@localhost.localdomain>
2023-08-07 18:57:36 +0000

    Added simple animation

--- src/main.c
+++ src/main.c
@@ -21,36 +21,17 @@ Rectangle RecV(Vector2 a, Vector2 size) {
   return (Rectangle){a.x, a.y, size.x, size.y};
 }
 
-
-// 2.7 -> -1.5
-// 2.7 + 1.5 = 4.2
-// 2.7 - 1.5 = 1.2
-float RotationLerp(float rot, float target, float mul) {
-    if (fabs(target + rot) > fabs(target - rot)) {
-        rot += mul * (target - rot);
-    } else {
-        rot += mul * (target + rot);
-    }
-    if (rot > PI) {
-        rot -= 2*PI;
-    } else if (rot < -PI) {
-        rot += 2*PI;
-    }
-    return rot;
-}
-
 void Move(ecs_iter_t *it) {
   Position *p = ecs_field(it, Position, 1);
   Velocity *v = ecs_field(it, Velocity, 2);
   Rotation *r = ecs_field(it, Rotation, 3);
 
   for (int i = 0; i < it->count; i++) {
-    p[i].x += v[i].x;
-    p[i].y += v[i].y;
+    p[i].x += v[i].x * it->delta_time;
+    p[i].y += v[i].y * it->delta_time;
 
     Rotation target = -Vector2Angle((Vector2){0, -1}, v[i]);
-    r[i] = RotationLerp(r[i], target, 0.3);
-    // r[i] = target;
+    r[i] = target;
   }
 }
 
@@ -58,19 +39,22 @@ void Draw(ecs_iter_t *it) {
   Position *p = ecs_field(it, Position, 1);
   Rotation *r = ecs_field(it, Rotation, 2);
   Scale *s = ecs_field(it, Scale, 3);
-  Texture *t = ecs_field(it, Texture, 4);
+  Animation *a = ecs_field(it, Animation, 4);
 
   for (int i = 0; i < it->count; i++) {
     char buf[255];
     sprintf(buf, "%f\n", r[i]);
     DrawText(buf, p[i].x, p[i].y - 100, 14, BLACK);
 
-    Vector2 size = Vector2Scale((Vector2){t[i].width, t[i].height}, s[i]);
+    Vector2 size = Vector2Scale((Vector2){a[i].frame_width, a[i].sheet.height}, s[i]);
     Vector2 halfsize = Vector2Scale(size, 0.5);
     
     Vector2 pos = Vector2Subtract(p[i], Vector2Rotate(halfsize, r[i]));
 
-    DrawTextureEx(t[i], pos, ToDeg(r[i]), s[i], WHITE);
+    Rectangle source = {a[i].cur_frame * a[i].frame_width, 0, a[i].frame_width, a[i].sheet.height};
+    Rectangle dest = {pos.x, pos.y, a[i].sheet.height * s[i], a[i].frame_width * s[i]};
+    DrawTexturePro(a[i].sheet, source, dest, Vector2Zero(), ToDeg(r[i]), WHITE);
+    a[i].cur_frame++;
 
     // DrawRectangleLinesEx(RecV(Vector2Subtract(p[i], halfsize), size), 5, RED);
   }
@@ -97,25 +81,32 @@ int main(void) {
   ECS_COMPONENT(ecs, Velocity);
   ECS_COMPONENT(ecs, Rotation);
   ECS_COMPONENT(ecs, Scale);
-  ECS_COMPONENT(ecs, Texture);
+  ECS_COMPONENT(ecs, Animation);
 
   ECS_SYSTEM(ecs, Move, EcsOnUpdate, Position, Velocity, Rotation);
-  ECS_SYSTEM(ecs, Draw, EcsOnUpdate, Position, Rotation, Scale, Texture);
+  ECS_SYSTEM(ecs, Draw, EcsOnUpdate, Position, Rotation, Scale, Animation);
 
   ecs_entity_t player = ecs_new_id(ecs);
   ecs_set(ecs, player, Position, {0, 0});
   ecs_set(ecs, player, Velocity, {0, 0});
   ecs_set(ecs, player, Rotation, {0});
   ecs_set(ecs, player, Scale, {5});
-  Texture arrow = LoadTexture(ASSET "Arrow.png");
-  ecs_set_ptr(ecs, player, Texture, &arrow);
+  
+  Animation starship = {
+      .sheet = LoadTexture(ASSET "starship.png"),
+      .cur_frame = 0,
+      .frame_width = 16,
+  };
+
+  ecs_set_ptr(ecs, player, Animation, &starship);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(WHITE);
     BeginMode2D(camera);
 
-    ecs_progress(ecs, GetFrameTime());
+    const float dt = GetFrameTime();
+    ecs_progress(ecs, dt);
 
     { // player movement
       static Velocity player_vel;
@@ -123,19 +114,19 @@ int main(void) {
       bool changed = false;
 
       if (IsKeyDown(KEY_RIGHT)) {
-        player_vel.x = Clamp(player_vel.x + 2, -3, 3);
+        player_vel.x = Clamp(player_vel.x + 200, -200, 200);
         changed = true;
       }
       if (IsKeyDown(KEY_LEFT)) {
-        player_vel.x = Clamp(player_vel.x - 2, -3, 3);
+        player_vel.x = Clamp(player_vel.x - 200, -200, 200);
         changed = true;
       }
       if (IsKeyDown(KEY_UP)) {
-        player_vel.y = Clamp(player_vel.y - 2, -3, 3);
+        player_vel.y = Clamp(player_vel.y - 200, -200, 200);
         changed = true;
       }
       if (IsKeyDown(KEY_DOWN)) {
-        player_vel.y = Clamp(player_vel.y + 2, -3, 3);
+        player_vel.y = Clamp(player_vel.y + 200, -200, 200);
         changed = true;
       }
 
@@ -161,7 +152,7 @@ int main(void) {
       camera.target = mouseWorldPos;
 
       // Zoom increment
-      const float zoomIncrement = 0.125f;
+      const float zoomIncrement = 50 * dt;
 
       camera.zoom += (wheel * zoomIncrement);
       if (camera.zoom < zoomIncrement)
