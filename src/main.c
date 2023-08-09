d262fee5b8de74019684706b89ba0f3a3b50c6c4
rndm <rndm@localhost.localdomain>
2023-08-09 10:35:25 +0000

    Added basic contact damage system

--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
 #include "flecs.h"
 #include "raylib.h"
 #include "raymath.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef Vector2 Velocity;
 typedef Vector2 Position;
+
 typedef float Rotation;
 typedef float Scale;
+
+typedef uint8_t Team;
+typedef int32_t Health;
+typedef int32_t ContactDamage;
 
 typedef struct Animation {
     Texture sheet;
@@ -25,54 +31,93 @@ float ToDeg(float rad) { return (180 / PI) * rad; }
 float ToRad(float deg) { return (PI / 180) * deg; }
 
 Rectangle RecV(Vector2 a, Vector2 size) {
-  return (Rectangle){a.x, a.y, size.x, size.y};
+    return (Rectangle){a.x, a.y, size.x, size.y};
 }
 
 void Move(ecs_iter_t *it) {
-  Position *p = ecs_field(it, Position, 1);
-  Velocity *v = ecs_field(it, Velocity, 2);
-  Rotation *r = ecs_field(it, Rotation, 3);
+    Position *p = ecs_field(it, Position, 1);
+    Velocity *v = ecs_field(it, Velocity, 2);
+    Rotation *r = ecs_field(it, Rotation, 3);
 
-  for (int i = 0; i < it->count; i++) {
-    p[i].x += v[i].x * it->delta_time;
-    p[i].y += v[i].y * it->delta_time;
+    for (int i = 0; i < it->count; i++) {
+        p[i].x += v[i].x * it->delta_time;
+        p[i].y += v[i].y * it->delta_time;
 
-    Rotation target = -Vector2Angle((Vector2){0, -1}, v[i]);
-    r[i] = target;
-  }
+        Rotation target = -Vector2Angle((Vector2){0, -1}, v[i]);
+        r[i] = target;
+    }
 }
 
-void Draw(ecs_iter_t *it) {
-  Position *p = ecs_field(it, Position, 1);
-  Rotation *r = ecs_field(it, Rotation, 2);
-  Scale *s = ecs_field(it, Scale, 3);
-  Animation *a = ecs_field(it, Animation, 4);
+void DrawAnimation(ecs_iter_t *it) {
+    Position *p = ecs_field(it, Position, 1);
+    Rotation *r = ecs_field(it, Rotation, 2);
+    Scale *s = ecs_field(it, Scale, 3);
+    Animation *a = ecs_field(it, Animation, 4);
+    // Health *h = ecs_field(it, Health, 5);
 
-  for (int i = 0; i < it->count; i++) {
-    // char buf[255];
-    // sprintf(buf, "%f\n", r[i]);
-    // DrawText(buf, p[i].x, p[i].y - 100, 14, BLACK);
+    for (int i = 0; i < it->count; i++) {
+        // char buf[255];
+        // sprintf(buf, "Health: %d\n", h[i]);
+        // DrawText(buf, p[i].x, p[i].y - 100, 14, RAYWHITE);
 
-    Vector2 size = Vector2Scale((Vector2){a[i].frame_width, a[i].sheet.height}, s[i]);
-    Vector2 halfsize = Vector2Scale(size, 0.5);
-    
-    Vector2 pos = Vector2Subtract(p[i], Vector2Rotate(halfsize, r[i]));
+        Vector2 size = Vector2Scale((Vector2){a[i].frame_width, a[i].sheet.height}, s[i]);
+        Vector2 halfsize = Vector2Scale(size, 0.5);
 
-    Rectangle source = {a[i].cur_frame * a[i].frame_width, 0, a[i].frame_width, a[i].sheet.height};
-    Rectangle dest = {pos.x, pos.y, a[i].sheet.height * s[i], a[i].frame_width * s[i]};
-    
-    DrawTexturePro(a[i].sheet, source, dest, Vector2Zero(), ToDeg(r[i]), WHITE);
-    
-    a[i].time += it->delta_time;
-    if (a[i].time > 1. / a[i].fps) {
-        a[i].time = 0;
-        a[i].cur_frame++;
-        a[i].cur_frame %= frame_count(a[i]);
+        Vector2 pos = Vector2Subtract(p[i], Vector2Rotate(halfsize, r[i]));
+
+        Rectangle source = {a[i].cur_frame * a[i].frame_width, 0, a[i].frame_width, a[i].sheet.height};
+        Rectangle dest = {pos.x, pos.y, a[i].sheet.height * s[i], a[i].frame_width * s[i]};
+
+        DrawTexturePro(a[i].sheet, source, dest, Vector2Zero(), ToDeg(r[i]), WHITE);
+
+        a[i].time += it->delta_time;
+        if (a[i].time > 1. / a[i].fps) {
+            a[i].time = 0;
+            a[i].cur_frame++;
+            a[i].cur_frame %= frame_count(a[i]);
+        }
+
+        // Bounding box
+        // DrawRectangleLinesEx(RecV(Vector2Subtract(p[i], halfsize), size), 5, RED);
     }
+}
 
-    // Bounding box
-    // DrawRectangleLinesEx(RecV(Vector2Subtract(p[i], halfsize), size), 5, RED);
-  }
+void Collisions(ecs_iter_t *it) {
+    Position *p = ecs_field(it, Position, 1);
+    // Rotation *r = ecs_field(it, Rotation, 2);
+    Scale *s = ecs_field(it, Scale, 2);
+    Animation *a = ecs_field(it, Animation, 3);
+
+
+    Health *h = ecs_field(it, Health, 4);
+    ContactDamage *d = ecs_field(it, ContactDamage, 5);
+    Team *t = ecs_field(it, Team, 6);
+
+    for (int i = 0; i < it->count; i++) {
+        Rectangle irec = RecV(p[i], Vector2Scale((Vector2){a[i].frame_width, a[i].sheet.height}, s[i]));
+        for (int j = i; j < it->count; j++) {
+            if (t[j] == t[i]) continue; // Skip if same teams
+                                        // Possibly remake this so teams would be split into different sections
+                                        // with a system that activates on set
+            
+            Rectangle jrec = RecV(p[j], Vector2Scale((Vector2){a[j].frame_width, a[j].sheet.height}, s[j]));
+            
+            if (CheckCollisionRecs(irec, jrec)) {
+                h[i] -= d[j];
+                h[j] -= d[i];
+            } // TODO: add iframes
+        }
+    }
+}
+
+void HealthCheck(ecs_iter_t *it) {
+    Health *h = ecs_field(it, Health, 1);
+
+    for (int i = 0; i < it->count; i++) {
+        if (h[i] <= 0) { // TODO: add death animation
+            ecs_delete(it->world, it->entities[i]);
+        }
+    }
 }
 
 int main(void) {
@@ -94,18 +139,22 @@ int main(void) {
 
     ECS_COMPONENT(ecs, Position);
     ECS_COMPONENT(ecs, Velocity);
+    
     ECS_COMPONENT(ecs, Rotation);
     ECS_COMPONENT(ecs, Scale);
+   
     ECS_COMPONENT(ecs, Animation);
+   
+    ECS_COMPONENT(ecs, Health);
+    ECS_COMPONENT(ecs, ContactDamage);
+    ECS_COMPONENT(ecs, Team);
 
-    ECS_SYSTEM(ecs, Move, EcsOnUpdate, Position, Velocity, Rotation);
-    ECS_SYSTEM(ecs, Draw, EcsOnUpdate, Position, Rotation, Scale, Animation);
-
-    ecs_entity_t player = ecs_new_id(ecs);
-    ecs_set(ecs, player, Position, {0, 0});
-    ecs_set(ecs, player, Velocity, {0, 0});
-    ecs_set(ecs, player, Rotation, {0});
-    ecs_set(ecs, player, Scale, {5});
+    ECS_SYSTEM(ecs, Move, EcsOnUpdate, Position, Velocity, Rotation); // Every update change position 
+    
+    ECS_SYSTEM(ecs, DrawAnimation, EcsOnUpdate, Position, Rotation, Scale, Animation); // Every update draw on screen
+    
+    ECS_SYSTEM(ecs, Collisions, EcsOnUpdate, Position, Scale, Animation, Health, ContactDamage, Team); // Only when position/scale is set
+    ECS_SYSTEM(ecs, HealthCheck, EcsOnUpdate, Health); // Only when health is set
 
     Animation a_starship = {
         .sheet = LoadTexture(ASSET "starship.png"),
@@ -127,12 +176,21 @@ int main(void) {
     Texture t_mg = LoadTexture(ASSET "Midground.png");
     Texture t_fg = LoadTexture(ASSET "Foreground.png");
 
+    ecs_entity_t player = ecs_new_id(ecs);
+    ecs_set(ecs, player, Position, {0, 0});
+    ecs_set(ecs, player, Velocity, {0, 0});
+    ecs_set(ecs, player, Rotation, {0});
+    ecs_set(ecs, player, Scale, {5});
+    ecs_set(ecs, player, Health, {5});
+    ecs_set(ecs, player, ContactDamage, {0});
+    ecs_set(ecs, player, Team, {0});
+
     ecs_set_ptr(ecs, player, Animation, &a_starship);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
         BeginMode2D(camera);
-       
+
         static Position player_pos;
         player_pos = *ecs_get(ecs, player, Position);
         static Velocity player_vel;
@@ -143,11 +201,11 @@ int main(void) {
         { // Backgrounds
             Position top = GetScreenToWorld2D((Vector2){-1, -1}, camera);
             Position bot = GetScreenToWorld2D((Vector2){GetScreenWidth(), GetScreenHeight()}, camera);
-            
+
             Position bg_offset = Vector2Scale(camera.target, -0.1);
             bg_offset.x = bg_offset.x - t_bg.width;
             bg_offset.y = bg_offset.y - t_bg.height;
-            
+
             for (int16_t x = bg_offset.x + top.x; x <= bot.x; x += t_bg.width) {
                 for (int16_t y = bg_offset.y + top.y; y <= bot.y; y += t_bg.height) {
                     DrawTextureEx(t_bg, (Vector2){x, y}, 0, 1, WHITE);
@@ -157,28 +215,28 @@ int main(void) {
             Position mg_offset = Vector2Scale(camera.target, -0.4);
             mg_offset.x = mg_offset.x - t_mg.width;
             mg_offset.y = mg_offset.y - t_mg.height;
-            
+
             for (int16_t x = mg_offset.x + top.x; x <= bot.x; x += t_mg.width) {
                 for (int16_t y = mg_offset.y + top.y; y <= bot.y; y += t_mg.height) {
                     DrawTextureEx(t_mg, (Vector2){x, y}, 0, 1, WHITE);
                 }
             }
-            
+
             Position fg_offset = Vector2Scale(camera.target, -0.9);
             fg_offset.x = fg_offset.x - t_fg.width;
             fg_offset.y = fg_offset.y - t_fg.height;
-            
+
             for (int16_t x = fg_offset.x + top.x; x <= bot.x; x += t_fg.width) {
                 for (int16_t y = fg_offset.y + top.y; y <= bot.y; y += t_fg.height) {
                     DrawTextureEx(t_fg, (Vector2){x, y}, 0, 1, WHITE);
                 }
             }
         }
-        
+
         const float dt = GetFrameTime();
         ecs_progress(ecs, dt); // Also draws every entity
 
-        { // player movement
+        { // player controls
             bool changed = false;
 
             if (IsKeyDown(KEY_RIGHT)) {
@@ -207,6 +265,49 @@ int main(void) {
 
 
             ecs_set_ptr(ecs, player, Velocity, &player_vel);
+
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                ecs_entity_t laser = ecs_new_id(ecs);
+
+                const Rotation* rot = ecs_get(ecs, player, Rotation);
+                ecs_set_ptr(ecs, laser, Rotation, rot);
+
+                Velocity vel = Vector2Rotate((Vector2){0, -500}, *ecs_get(ecs, player, Rotation));
+                ecs_set_ptr(ecs, laser, Velocity, &vel);
+
+                Velocity init_vel = Vector2Rotate((Vector2){0, -3 * a_starship.sheet.height}, *ecs_get(ecs, player, Rotation));
+                Position pos = Vector2Add(init_vel, *ecs_get(ecs, player, Position));
+                ecs_set_ptr(ecs, laser, Position, &pos);
+
+                ecs_set(ecs, laser, Scale, {3});
+                
+                ecs_set(ecs, laser, Health, {1 << 15});
+                ecs_set(ecs, laser, ContactDamage, {1});
+                ecs_set(ecs, laser, Team, {0});
+
+                ecs_set_ptr(ecs, laser, Animation, &a_laser);
+            }
+
+            if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+                ecs_entity_t enemy = ecs_new_id(ecs);
+
+                const Rotation* rot = 0;
+                ecs_set_ptr(ecs, enemy, Rotation, rot);
+
+                Velocity vel = {0, 0};
+                ecs_set_ptr(ecs, enemy, Velocity, &vel);
+
+                Position pos = GetScreenToWorld2D(GetMousePosition(), camera);
+                ecs_set_ptr(ecs, enemy, Position, &pos);
+
+                ecs_set(ecs, enemy, Scale, {3});
+                
+                ecs_set(ecs, enemy, Health, {5});
+                ecs_set(ecs, enemy, ContactDamage, {1});
+                ecs_set(ecs, enemy, Team, {1});
+
+                ecs_set_ptr(ecs, enemy, Animation, &a_starship);
+            }
         }
 
         { // Camera
@@ -217,30 +318,10 @@ int main(void) {
             camera.zoom = Clamp(camera.zoom, 0.1, 5);
         }
 
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            ecs_entity_t laser = ecs_new_id(ecs);
-
-            const Rotation* rot = ecs_get(ecs, player, Rotation);
-            ecs_set_ptr(ecs, laser, Rotation, rot);
-            
-            Velocity vel = Vector2Rotate((Vector2){0, -500}, *ecs_get(ecs, player, Rotation));
-            ecs_set_ptr(ecs, laser, Velocity, &vel);
-            
-            Velocity init_vel = Vector2Rotate((Vector2){0, -3 * a_starship.sheet.height}, *ecs_get(ecs, player, Rotation));
-            Position pos = Vector2Add(init_vel, *ecs_get(ecs, player, Position));
-            ecs_set_ptr(ecs, laser, Position, &pos);
-            
-            ecs_set(ecs, laser, Scale, {3});
-
-            ecs_set_ptr(ecs, laser, Animation, &a_laser);
-        }
-
         EndMode2D();
 
         { // UI
             DrawFPS(5, 5);
-
-
         }
         EndDrawing();
     }
